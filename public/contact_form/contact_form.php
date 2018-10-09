2032ea3f19e5e1336b26c6a0f0a21575633d0750
Abhinava <reltronx@gmail.com>
2018-10-10 02:08:22 +0530

    modified the contact form php script and changed CV format to pdf

--- public/contact_form/contact_form.php
+++ public/contact_form/contact_form.php
@@ -2,7 +2,7 @@
 
 // configure
 $from = 'Contact form www.abhinava.in';
-$sendTo = 'abhinava.sanyal@gmail.com';
+$sendTo = 'abhinava.sanyal16@gmail.com';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
